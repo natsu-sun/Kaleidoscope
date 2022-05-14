28b894040a2f70923626e8fefc213e50e01bc734
natsu <38107965+natsu-sun@users.noreply.github.com>
2022-05-15 01:21:30 +0900

    回転をキャンバスではなく文字に対して行うようにした

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+// 万華鏡キャンバス
 const cv = document.getElementById('cv')
 cv.width = cv.height = 400
-
 const ctx = cv.getContext("2d")
-ctx.textBaseline = "middle"
-ctx.textAlign = "center"
+// 文字キャンバス
+const tcv = document.createElement("canvas")
+tcv.width = tcv.height = 400
+const tctx = tcv.getContext("2d")
+tctx.textBaseline = "middle"
+tctx.textAlign = "center"
 
 document.getElementById('text').addEventListener('input', refresh)
 
@@ -17,31 +21,34 @@ refresh()
 
 
 function refresh() {
+    // 文字生成
     const text = document.getElementById('text').value
     const sz = +document.getElementById('size').value
+    const r = +document.getElementById('r').value
+
+    tctx.font = "bold " + sz + "px sans-serif"
+    tctx.resetTransform()
+    tctx.clearRect(0, 0, tcv.width, tcv.height)
+    tctx.translate(tcv.width/2, tcv.height/2)
+    tctx.rotate(r * Math.PI / 180)
+    tctx.fillText(text, 0, 0)
+
+    // 万華鏡生成
     const x = +document.getElementById('x').value
     const y = +document.getElementById('y').value
-    const r = +document.getElementById('r').value
     const n = +document.getElementById('n').value
 
     const ang = 360 / n
     const deg = ang * Math.PI / 180
 
-    ctx.font = "bold " + sz + "px sans-serif"
-
     ctx.resetTransform()
     ctx.clearRect(0, 0, cv.width, cv.height)
     
     ctx.translate(cv.width/2, cv.height/2)
-    ctx.rotate(r * Math.PI / 180)
     for (let i=0; i<n; i++) {
         ctx.rotate(deg)
-        ctx.fillText(text, x, y)
-      }
-}
-
-function getValueById(id) {
-    return document.getElementById(id).value
+        ctx.drawImage(tcv, x-cv.width/2, y-cv.height/2)
+    }
 }
 
 // inputイベント時に値をセットする関数
